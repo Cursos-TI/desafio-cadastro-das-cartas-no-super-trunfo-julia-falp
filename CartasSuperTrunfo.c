5ae7d6f6536a934eafd3619553082397c3e939cb
Julia Falp <julia.falquete@gmail.com>
2025-04-06 14:07:17 -0300

    Adicionar código das cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
-
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+#include <locale.h>
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    setlocale(LC_ALL, "Portuguese");
+    char estado;
+    char codigoDaCarta[50], nomeDaCidade[50];
+    int populacao, nPontoTuristico;
+    float area, pib;
 
+    printf("Digite a primeira letra do Estado de A a H: \n");
+    scanf("%c", &estado);
+    printf("Digite o c�digo da carta de 01 a 04: \n");
+    scanf("%s", &codigoDaCarta);
+    printf("Digite o nome da cidade: \n");
+    scanf("%s", &nomeDaCidade);
+    printf("Digite a popula��o: \n");
+    scanf("%i", &populacao);
+    printf("Digite a �rea: \n");
+    scanf("%f", &area);
+    printf("Digite o PIB: \n");
+    scanf("%f", &pib);
+    printf("Digite o n�mero de pontos tur�sticos: \n");
+    scanf("%i", &nPontoTuristico);
+    printf("Carta 1: \nEstado: %c\nC�digo: %c%s\nNome da Cidade: %s\nPopula��o: %i \n�rea: %.2f km�\nPIB: %.2f bilh�es de reais\nN�mero de pontos tur�sticos: %i\n", estado, estado, codigoDaCarta, nomeDaCidade, populacao, area, pib, nPontoTuristico);
     return 0;
 }
